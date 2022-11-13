f652df2588e3072ba7e9b19d2f1125ce694ac69a
Velikiy <weather_moonlight_1996@mail.ru>
2022-11-13 22:11:53 +0500

    report_product

---
+++ report_config/report_app_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReportApp1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'report_app_1'
---
+++ report_config/report_app_1/models.py
@@ -0,0 +1,43 @@
+import datetime
+
+from django.db import models
+
+
+# Create your models here.
+
+class product(models.Model):
+    sinf1 = 'I-sinf'
+    sinf2 = 'II-sinf'
+    sinf3 = 'III-sinf'
+    sinf4 = 'IV-sinf'
+    sinf5 = 'V-sinf'
+    ROLES = (
+        (sinf1, "I-sinf"),
+        (sinf2, "II-sinf"),
+        (sinf3, "III-sinf"),
+        (sinf4, "IV-sinf"),
+        (sinf5, "V-sinf"),
+    )
+    Mahsulot_nomi = models.CharField(max_length=256)
+    Shifr = models.CharField(max_length=128)
+    Sinf = models.CharField(max_length=30, choices=ROLES)
+    Texnik_shart_seriyasi = models.CharField(max_length=16)
+    Texnik_shart_raqami = models.CharField(max_length=32)
+    Sana = models.DateTimeField(default=datetime.datetime.now())
+    Sinov_namunasi = models.BooleanField(default=False)
+    Partiya_soni_belgilash = models.IntegerField()
+    Partiya_raqami = models.IntegerField()
+    Buyurtmachi_nomi = models.CharField(max_length=128)
+    Buyurtmachi_raqami = models.IntegerField()
+    Buyurtma_soni = models.IntegerField()
+    Buyurtma_muddati = models.DateTimeField()
+    Sinov_bayonnomasi_raqami = models.CharField(max_length=8)
+    Sinov_bayonnomasi_sanasi = models.DateTimeField()
+    Sinov_qilingan_mahsulot_nomi = models.CharField(max_length=128)
+    Sinov_bayonnomasi_registratsiya_raqami = models.CharField(max_length=8)
+
+    class Meta:
+        db_table = "product"
+
+    def __str__(self):
+        return f"{self.Mahsulot_nomi}"
